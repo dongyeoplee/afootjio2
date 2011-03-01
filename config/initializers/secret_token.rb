750324961e0fa6de9fce206af99802809a429e3a
afootjio <dongyeop.lee@headflow.net>
2011-03-01 17:00:45 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afootjio2::Application.config.secret_token = '06851232e3c2174b35f63b2c07f53432089196c3d5cad3d264af575f9f49eed29505273f0e4a80119ae63ddef9c91e4a5d28d9176f752a67909d4250508c823e'
